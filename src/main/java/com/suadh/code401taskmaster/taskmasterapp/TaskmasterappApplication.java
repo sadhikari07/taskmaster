6ae5cf1dce0072d3aa1627aca31125deb7b6a9c8
Sudip Adhikari <sudeep_sudeep07@hotmail.com>
2019-07-01 21:24:04 -0700

    initial commit, setup repo

---
+++ src/main/java/com/suadh/code401taskmaster/taskmasterapp/TaskmasterappApplication.java
@@ -0,0 +1,13 @@
+package com.suadh.code401taskmaster.taskmasterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmasterappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmasterappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suadh/code401taskmaster/taskmasterapp/TaskmasterappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.suadh.code401taskmaster.taskmasterapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskmasterappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
